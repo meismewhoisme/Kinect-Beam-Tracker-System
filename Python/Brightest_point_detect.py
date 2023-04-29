6b227a7158d35ea649a02dbda8771ddd9a80641d
meismewhoisme <Geive.anwar@gmail.com>
2023-04-29 21:03:03 +0800

    Newest change

---
+++ Python/Brightest_point_detect.py
@@ -0,0 +1,46 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-r", "--radius", type = int,
+	help = "radius of Gaussian blur; must be odd")
+args = vars(ap.parse_args())
+# load the image and convert it to grayscale
+
+##### brightest point without any gaussian blur#########
+image = cv2.imread(args["image"])
+orig = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# perform a naive attempt to find the (x, y) coordinates of
+# the area of the image with the largest intensity value
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
+# display the results of the naive attempt
+
+#cv2.imshow("Naive", image)
+
+
+##### brightest point with gaussian blur#########
+# apply a Gaussian blur to the image then find the brightest region
+
+
+gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
+#finds pixel withhighest value in grey image
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+image = orig.copy()
+cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
+# display the results of our newly improved method
+
+#cv2.imshow("Robust", image)
+#cv2.waitKey(0)
+
+print(maxLoc)
+
+
+
+filename = r"C:\Users\geive\Desktop\engineering y12\Code\Made\Edited IR basics\Images\Analysed\Done.png"
+cv2.imwrite(filename, image)
---
+++ Python/Mousemover.py
@@ -0,0 +1,62 @@
+# moves the mouse to a set position
+#from screeninfo import get_monitors
+import mouse
+import argparse
+import inspect
+
+#Primemon = 0
+#monwidth = ""
+#monheight = ""
+
+#for m in get_monitors():
+ #   string_monitor = str(m)
+   # isprimary = string_monitor.split("is_primary=", 1)[1]
+    
+ #   if (isprimary == "True)"):
+   #     Primemon = m
+#        string_monitor = str(m)
+ #       monwidth = string_monitor.split("width=", 1)[1]
+ #       monwidth = monwidth[:4]
+#
+ #       monheight = string_monitor.split("height=", 1)[1]
+#        monheight = monheight[:4]
+
+
+        
+
+#monwidth = int(monwidth)
+#monheight = int(monheight)
+import ctypes
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+try:
+    monwidth = screensize[0]
+    monheight = screensize[0]
+except:
+    monwidth = 1920
+    monheight = 1080
+
+    
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-xcoor", "--X_Coordinate", type = int, help = "X coordinate on the screen")
+ap.add_argument("-ycoor", "--Y_Coordinate", type = int, help = "Y coordinate on the screen")
+args = vars(ap.parse_args())
+
+x = args["X_Coordinate"]
+y = args["Y_Coordinate"]
+
+x = x/640 
+
+x = x * monwidth
+x = round(x)
+print("x pos: " + str(x) )
+
+#y = 480 - y
+y = y/480 
+y = y * monheight
+y = round(y)
+print("y pos: " + str(y) )
+
+mouse.move(x, y, absolute=True, duration=0)
