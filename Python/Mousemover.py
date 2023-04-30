cd9146556f40f46b2b9fa360e2619312ece49a34
meismewhoisme <90593003+meismewhoisme@users.noreply.github.com>
2023-04-30 18:15:18 +0800

    Finished python side accomodations for calibration

--- Python/Mousemover.py
+++ Python/Mousemover.py
@@ -9,7 +9,7 @@ screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
 try: #tries to set it to the set values
     monwidth = screensize[0]
-    monheight = screensize[0]
+    monheight = screensize[1]
 except: #if this fails it sets the width and height to these default values
     monwidth = 1920     #resolution width of monitor being used (change if resolution size changes)
     monheight = 1080    #resolution height of monitor being used (change if resolution size changes)
@@ -37,39 +37,43 @@ try:
 
 
     # reading data
-    config_data.read("config/config.ini")
+    config_data.read("config.ini")
 
 
     # app configuration data
     mouse_movement = config_data["mouse_movement"]
 
 
-    print("mouse_movement data")
     for mouse_movement_data in mouse_movement:
-        print(f"{mouse_movement_data} = {mouse_movement.get(mouse_movement_data)}")
         if mouse_movement_data == "x_start_pos":
-            x_start_pos = mouse_movement.get(mouse_movement_data)
+            x_pos_start = int(mouse_movement.get(mouse_movement_data))
         elif mouse_movement_data == "x_end_pos":
-            x_end_pos = mouse_movement.get(mouse_movement_data)
+            x_pos_end = int(mouse_movement.get(mouse_movement_data))
         elif mouse_movement_data == "y_start_pos":
-            y_start_pos = mouse_movement.get(mouse_movement_data)
+            y_pos_start = int(mouse_movement.get(mouse_movement_data))
         elif mouse_movement_data == "y_end_pos":
-            y_end_pos = mouse_movement.get(mouse_movement_data)
+            y_pos_end = int(mouse_movement.get(mouse_movement_data))
 
-    cam_width = x_pos_end - x_pos_start 
+    cam_width = x_pos_end - x_pos_start
+    
     cam_height = y_pos_end - y_pos_start
+    
 
     x = x - x_pos_start
     y = y - y_pos_start
 
+    
 
 except:
+
     cam_width = 640
     cam_height = 480
 
+#end tab
 
 x = x/cam_width
 y = y/cam_height
+
 
 
 x = x * monwidth
